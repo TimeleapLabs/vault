2e69e42689644220708ffb374939156493effe64
Pouya <pouya.eghbali@yandex.com>
2022-02-01 23:20:07 +0100

    First commit

---
+++ meta/constants.js
@@ -0,0 +1,4 @@
+export const EC2_METADATA_URL = "http://169.254.169.254/latest/";
+export const DEFAULT_WRAP_TTL = "120s";
+export const STS_BODY = "Action=GetCallerIdentity&Version=2011-06-15";
+export const STS_URL = "https://sts.amazonaws.com/";
---
+++ meta/parameters.js
@@ -0,0 +1,2 @@
+export const VAULT_ADDR = process.env.VAULT_ADDR;
+export const AWS_ROLE = process.env.VAULT_AWS_ROLE;
