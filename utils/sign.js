8997c0a16e430e2a7bb9ef852f20b3dd5ecf74ff
Pouya <pouya.eghbali@yandex.com>
2022-02-02 22:38:41 +0100

    Minor refactor

--- utils/sign.js
+++ utils/sign.js
@@ -1,26 +1,14 @@
 import aws4 from "aws4";
 import { STS_BODY } from "../meta/constants.js";
 
-export const signWithServerId = (awsServerId, options = {}) =>
-  aws4.sign({
+export const sign = (awsServerId, credentials) => {
+  const request = {
     service: "sts",
-    region: "eu-central-1",
-    doNotModifyHeaders: false,
-    headers: { "X-Vault-AWS-IAM-Server-ID": awsServerId },
     body: STS_BODY,
-    ...options,
-  });
-
-export const signNoServerId = (options = {}) =>
-  aws4.sign({
-    service: "sts",
-    region: "eu-central-1",
-    doNotModifyHeaders: false,
-    body: STS_BODY,
-    ...options,
-  });
-
-export const sign = (awsServerId, options) =>
-  awsServerId
-    ? signWithServerId(awsServerId, options)
-    : signNoServerId(options);
+    headers: {},
+  };
+  if (awsServerId) {
+    request.headers["X-Vault-AWS-IAM-Server-ID"] = awsServerId;
+  }
+  return aws4.sign(request, credentials);
+};
